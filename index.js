982a886f2eef7249caf1cb41a2cb6d57689830ca
ameisso <ameisso@gmail.com>
2022-07-24 15:37:51 +0200

    New : manage curve types

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ console.log("\n---------------------\n this files loads a vezer composition and 
 var fs = require('fs');
 const xml = require("xml-parse");
 
-
-
 fs.readFile('Untitled.xml', 'utf8', (err, data) => {
     if (err) {
         console.error(err);
@@ -24,13 +22,13 @@ fs.readFile('Untitled.xml', 'utf8', (err, data) => {
         var keyframes = track.getElementsByTagName("keyframes");
         var keyframesArray = keyframes[0].getElementsByTagName("keyframe");
 
-        var tweenString = "//Tween Timeline generated automatically \nTween::Timeline timeline;       // create timeline\ntimeline.mode(Tween::Mode::REPEAT_TL);\ntimeline.start();\ntimeline.add(target)            // add sequence to timeline"
+        var tweenString = "//Tween Timeline generated automatically \nTween::Timeline timeline;       // create timeline\ntimeline.mode(Tween::Mode::REPEAT_TL);\ntimeline.start();\ntimeline.add(target)            // add sequence to timeline\n"
         // console.log(keyframesArray);
         keyframesArray.forEach((item) => {
-
             var time = 0;
             var value = 0;
-            var type = 0;
+            var type = '';
+            var tweenType = '';
             timePoints = item.getElementsByTagName("time");
             timePoints.forEach((timepoint) => {
                 time = timepoint.innerXML;
@@ -42,14 +40,16 @@ fs.readFile('Untitled.xml', 'utf8', (err, data) => {
 
             curveTypes = item.getElementsByTagName("interpolation");
             curveTypes.forEach((curveType) => {
+
                 type = curveType.innerXML
+                tweenType = getTweenEasingName(type);
             });
             var timeSeconds = time / fps;
             var timeMs = Math.round(timeSeconds * 1000);
-            console.log("[" + timeSeconds + "] " + value + " " + type)
-            console.log("-----")
-
-            tweenString += ".then<Ease::Sine>(" + value + "," + timeMs + ",[]() {})\n";
+            // console.log("[" + timeSeconds + "] " + value + " " + type)
+            // console.log("-----")
+            // tweenString += ".then<Ease::Sine>(" + value + "," + timeMs + ",[]() {})\n";
+            tweenString += ".then" + tweenType + "(" + value + "," + timeMs + ",[]() {})\n";
         });
         fs.writeFile(outputFileName, "\n" + tweenString + ";", function (err) {
             if (err) throw err;
@@ -57,3 +57,106 @@ fs.readFile('Untitled.xml', 'utf8', (err, data) => {
     });
 });
 
+function getTweenEasingName(vezerName) {
+    if (vezerName == "none") {
+        //warning none should be an offet 
+        return "";
+    }
+    else if (vezerName == "linear") {
+        return "";
+    }
+    else if (vezerName == "quadratic-in-out") {
+        return "<Ease::Quad>"
+    }
+    else if (vezerName == "cubic-in-out") {
+        return "<Ease::Cubic>"
+    }
+    else if (vezerName == "quartic-in-out") {
+        return "<Ease::Quart>"
+    }
+    else if (vezerName == "quintic-in-out") {
+        return "<Ease::Quint>"
+    }
+    else if (vezerName == "sine-in-out") {
+        return "<Ease::Sine>"
+    }
+    else if (vezerName == "circular-in-out") {
+        return "<Ease::Circ>"
+    }
+    else if (vezerName == "exponential-in-out") {
+        return "<Ease::Expo>"
+    }
+    else if (vezerName == "elastic-in-out") {
+        return "<Ease::Elastic>"
+    }
+    else if (vezerName == "back-in-out") {
+        return "<Ease::Back>"
+    }
+    else if (vezerName == "bounce-in-out") {
+        return "<Ease::Bounce>"
+    }//INs
+    else if (vezerName == "quadratic-in") {
+        return "<Ease::QuadIn>"
+    }
+    else if (vezerName == "cubic-in") {
+        return "<Ease::CubicIn>"
+    }
+    else if (vezerName == "quartic-in") {
+        return "<Ease::QuartIn>"
+    }
+    else if (vezerName == "quintic-in") {
+        return "<Ease::QuintIn>"
+    }
+    else if (vezerName == "sine-in") {
+        return "<Ease::SineIn>"
+    }
+    else if (vezerName == "circular-in") {
+        return "<Ease::CircIn>"
+    }
+    else if (vezerName == "exponential-in") {
+        return "<Ease::ExpoIn>"
+    }
+    else if (vezerName == "elastic-in") {
+        return "<Ease::ElasticIn>"
+    }
+    else if (vezerName == "back-in") {
+        return "<Ease::BackIn>"
+    }
+    else if (vezerName == "bounce-in") {
+        return "<Ease::BounceIn>"
+    }//Outs
+    else if (vezerName == "quadratic-out") {
+        return "<Ease::QuadOut>"
+    }
+    else if (vezerName == "cubic-out") {
+        return "<Ease::CubicOut>"
+    }
+    else if (vezerName == "quartic-out") {
+        return "<Ease::QuartOut>"
+    }
+    else if (vezerName == "quintic-out") {
+        return "<Ease::QuintOut>"
+    }
+    else if (vezerName == "sine-out") {
+        return "<Ease::SineOut>"
+    }
+    else if (vezerName == "circular-out") {
+        return "<Ease::CircOut>"
+    }
+    else if (vezerName == "exponential-out") {
+        return "<Ease::ExpoOut>"
+    }
+    else if (vezerName == "elastic-out") {
+        return "<Ease::ElasticOut>"
+    }
+    else if (vezerName == "back-out") {
+        return "<Ease::BackOut>"
+    }
+    else if (vezerName == "bounce-out") {
+        return "<Ease::BounceOut>"
+    }
+    else {
+        console.log("unknown type " + vezerName);
+        return "<Ease::Quad>"
+    }
+}
